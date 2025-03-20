9d858ad5a333649a946807d6def59582fa0e892b
Rachel <rachelzeng1231@gmail.com>
2025-03-19 23:25:34 -0700

    Initial Commit

---
+++ AvalonAI/AvalonAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvalonAIApp.swift
+//  AvalonAI
+//
+//  Created by Rachel on 2025-03-19.
+//
+
+import SwiftUI
+
+@main
+struct AvalonAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvalonAITests/AvalonAITests.swift
@@ -0,0 +1,17 @@
+//
+//  AvalonAITests.swift
+//  AvalonAITests
+//
+//  Created by Rachel on 2025-03-19.
+//
+
+import Testing
+@testable import AvalonAI
+
+struct AvalonAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
